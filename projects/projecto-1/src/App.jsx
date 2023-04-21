88820dedb1315af915ab0ae91bdbbc84bc1ff5ad
diegoalonsonm <diegoalonsonm@gmail.com>
2023-04-21 16:50:13 -0600

    creacion de un Twitter Follow Card clon

--- projects/projecto-1/src/App.jsx
+++ projects/projecto-1/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {TwitterFollowCard} from "./TwitterFollowCard.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export function App(){
+    // const formattedUsername = <span>@hpola</span>
+    const eaSports = {username:'easportsfifa', name: 'EAFC 24'}
+    return(
+        <section className="App">
+            <TwitterFollowCard username="diegoalonsonm" name="Diego Naranjo"/>
+            <TwitterFollowCard username="djmariio" name="DjMaRiiO"/>
+            <TwitterFollowCard {...eaSports}>
+            </TwitterFollowCard>
+            <TwitterFollowCard username="ye" name="Ye"/>
+        </section>
+    )
 }
-
-export default App
---
+++ projects/projecto-1/src/TwitterFollowCard.jsx
@@ -0,0 +1,30 @@
+import {useState} from "react";
+
+export function TwitterFollowCard({username='null', name='null'}) {
+    const [isFollowing, setIsFollowing] = useState(false)
+
+    const buttonText = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+
+    const toggleFollow = () => {
+        setIsFollowing(!isFollowing)
+    }
+
+    return (
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img className='tw-followCard-img' src={`https://unavatar.io/${username}`} alt="avatar"/>
+                <div className='tw-followCard-info'>
+                    <strong>{name}</strong>
+                    <span className='tw-followCard-span'>@{username}</span>
+                </div>
+            </header>
+            <aside>
+                <button className={buttonClassName} onClick={toggleFollow}>
+                    {buttonText}
+                </button>
+            </aside>
+
+        </article>
+    )
+}
