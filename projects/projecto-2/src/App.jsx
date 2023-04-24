bc54890f9065376f7a7708ac8a4475758b124cb9
diegoalonsonm <diegoalonsonm@gmail.com>
2023-04-24 11:36:47 -0600

    componentizacion

--- projects/projecto-2/src/App.jsx
+++ projects/projecto-2/src/App.jsx
@@ -1,33 +1,10 @@
 import './App.css'
 import {useState} from "react";
-
-const turns = {
-    X: 'x',
-    O: 'o'
-}
-
-const Square = ({ children, isSelected, updateBoard, index}) => {
-    const className = `square ${isSelected ? 'is-selected' : ''}`
-    const handleClick = () => {
-        updateBoard(index)
-    }
-    return (
-        <div onClick={handleClick} className={className}>
-            {children}
-        </div>
-    )
-}
-
-const winner_combos = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-]
+import confetti from "canvas-confetti"
+import {Square} from "./components/Square.jsx";
+import {turns, winner_combos} from "./constants.js";
+import {WinnerModal} from "./components/Winner.jsx";
+import {Board} from "./components/Board.jsx";
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
@@ -69,6 +46,7 @@ function App() {
 
       const newWinner = checkWinner(newBoard)
       if (newWinner) {
+          confetti()
           setWinner(newWinner)
       } else if (checkEndGame(newBoard)) {
           setWinner(false)
@@ -79,17 +57,7 @@ function App() {
       <main className="board">
           <h1>Tic Tac Toe</h1>
           <button onClick={resetGame}>Reset</button>
-          <section className="game">
-              {
-                  board.map((_, index) => {
-                      return (
-                          <Square key={index} index={index} updateBoard={updateBoard}>
-                              {board[index]}
-                          </Square>
-                      )
-                  })
-              }
-          </section>
+          <Board updateBoard={updateBoard} board={board}/>
           <section className="turn">
               <Square isSelected={turn === turns.X}>
                   {turns.X}
@@ -98,25 +66,7 @@ function App() {
                   {turns.O}
               </Square>
           </section>
-          {
-              winner !== null && (
-                  <section className="winner">
-                      <div className="text">
-                          <h2>
-                              {
-                                  winner === false ? 'Empate' : 'Ganó'
-                              }
-                          </h2>
-                          <header className="win">
-                              {winner && <Square>{winner}</Square>}
-                          </header>
-                          <footer>
-                              <button onClick={resetGame}>empezar de nuevo</button>
-                          </footer>
-                      </div>
-                  </section>
-              )
-          }
+          <WinnerModal resetGame={resetGame} winner={winner}/>
       </main>
   )
 }
---
+++ projects/projecto-2/src/components/Board.jsx
@@ -0,0 +1,17 @@
+import {Square} from "./Square.jsx";
+
+export const Board = ({updateBoard, board}) => {
+    return (
+        <section className="game">
+            {
+                board.map((_, index) => {
+                    return (
+                        <Square key={index} index={index} updateBoard={updateBoard}>
+                            {board[index]}
+                        </Square>
+                    )
+                })
+            }
+        </section>
+    )
+}
